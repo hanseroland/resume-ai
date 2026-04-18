3beb0fd664ec8c22318078ed1fd8c5539b3b24a1
hanseroland <rolandntougou@gmail.com>
2026-04-18 01:30:23 +0000

    refactor : rename errorMiddleware =>rrorHandler

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const cookieParser = require("cookie-parser");
-const errorHandler = require('./middlewares/errorMiddleware');
+const errorHandler = require('./middlewares/errorHandler');
 
 
 dotenv.config();
